7914ab7a4a224524d874a982c225c234dd57f1a8
MoonJeong <dev.moonjeong@gmail.com>
2025-09-26 15:58:30 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/study/gitpractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.gitpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
